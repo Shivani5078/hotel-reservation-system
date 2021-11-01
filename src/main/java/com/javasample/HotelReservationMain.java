416613d598cedb398b0ed0b0e7858adf1a5e25e6
shivani5078 <pawarshivani732@gmail.com>
2021-11-01 21:13:01 +0530

    Add:Initial Commit

---
+++ src/main/java/com/javasample/HotelReservationMain.java
@@ -0,0 +1,20 @@
+/******************************************************************************************************************
+ *
+ *  Start with Displaying Welcome to Hotel Reservation Program in HotelReservation class
+ *  Ability to add Hotel in a Hotel Reservation System with Name and rates for Regular Customer...
+ *
+ * @author : Shivani Pawar
+ * @Since  : 01-11-2021
+ *
+ ******************************************************************************************************************/
+package com.javasample;
+
+public class HotelReservationMain {
+
+	public static void main(String[] args) {
+
+		System.out.println(" Welcome To Hotel Reservation");
+	
+	}
+
+}
